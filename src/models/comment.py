90d2d6edc4b48e4bf60ca4137b65ca451ce23a0c
thalesassiscarvalho <thales.assis.carvalho@gmail.com>
2025-06-08 02:37:50 -0300

    Update comment.py

--- src/models/comment.py
+++ src/models/comment.py
@@ -1,4 +1,4 @@
-# src/models/comment.py
+# ARQUIVO: src/models/comment.py
 
 from src.models.user import db
 import datetime
@@ -7,11 +7,9 @@ class UserComment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
 
-    # Relações
     user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
     law_id = db.Column(db.Integer, db.ForeignKey("law.id", ondelete="CASCADE"), nullable=False)
 
-    # A "Âncora" do Comentário
     anchor_paragraph_id = db.Column(db.String(50), nullable=False)
 
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
