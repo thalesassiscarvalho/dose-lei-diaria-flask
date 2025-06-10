c484cecbf6edae1698e91714d7d8a1e33238295d
thalesassiscarvalho <thales.assis.carvalho@gmail.com>
2025-06-09 22:49:20 -0300

    Update notes.py

--- src/models/notes.py
+++ src/models/notes.py
@@ -4,17 +4,17 @@ import datetime
 
 class UserNotes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    # MODIFICADO: Adicionado ondelete="CASCADE"
     user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
     law_id = db.Column(db.Integer, db.ForeignKey("law.id", ondelete="CASCADE"), nullable=False)
     content = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
-    # Define relationships
-    user = db.relationship("User", backref=db.backref("notes", lazy=True))
-    law = db.relationship("Law", backref=db.backref("notes", lazy=True))
+    user = db.relationship("User", backref=db.backref("notes", lazy=True, cascade="all, delete-orphan"))
+    # MODIFICADO: Adicionado cascade="all, delete-orphan"
+    law = db.relationship("Law", backref=db.backref("law_notes", lazy=True, cascade="all, delete-orphan"))
 
-    # Unique constraint to prevent duplicate notes entries for the same user and law
     __table_args__ = (db.UniqueConstraint("user_id", "law_id", name="_user_law_notes_uc"),)
 
     def __repr__(self):
