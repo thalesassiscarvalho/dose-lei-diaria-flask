ff1d1fcd3a9c21cc02a9bb5ce588a705c56b60ea
Ubuntu <ubuntu@ip-172-31-47-25.us-west-2.compute.internal>
2025-06-11 00:38:51 +0000

    Fix down_revision for reset migration

---
+++ migrations/versions/20250610_reset_markup_reset_user_law_markup.py
@@ -0,0 +1,24 @@
+"""reset user_law_markup
+
+Revision ID: 20250610_reset_markup
+Revises: 20250610_diff_markups
+Create Date: 2025-06-11 00:29:44.848505
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '20250610_reset_markup'
+down_revision = 'bcd73b762318'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
