ece114de6c2f97078acf7c9f52c0eb22e37dc10e
thalesassiscarvalho <thales.assis.carvalho@gmail.com>
2025-06-22 21:35:04 -0300

    Add files via upload

---
+++ src/extensions.py
@@ -0,0 +1,14 @@
+# src/extensions.py
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+
+# Crie as instâncias aqui, sem associá-las a nenhuma app ainda.
+db = SQLAlchemy()
+mail = Mail()
+login_manager = LoginManager()
+migrate = Migrate()
+csrf = CSRFProtect()
