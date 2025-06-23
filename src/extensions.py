fca90c7a3a32672a83291ee48c5e714da5a5b53f
thalesassiscarvalho <thales.assis.carvalho@gmail.com>
2025-06-23 11:17:23 -0300

    Update extensions.py

--- src/extensions.py
+++ src/extensions.py
@@ -7,12 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
+from flask_mail import Mail  # <<< ADICIONADO
 
 # Instancia as extensões
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
 login_manager = LoginManager()
+mail = Mail()  # <<< ADICIONADO
 
 # Configura o login_manager
 login_manager.login_view = 'auth.login'
